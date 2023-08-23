e2d9f8776aaf27828d1214327ba306792c43786b
alirzaMhd <71058261+alirzaMhd@users.noreply.github.com>
2023-08-23 19:07:16 +0430

    theme done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:period_tracker/core/theme/theme.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,10 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.theme,
       home: Container()
     );
   }
