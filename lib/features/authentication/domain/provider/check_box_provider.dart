a5eafe3ae89d02cf9151feae21328fe61784b4ad
alirzaMhd <71058261+alirzaMhd@users.noreply.github.com>
2023-08-29 15:24:50 +0430

    added use_cases

--- lib/features/authentication/domain/provider/check_box_provider.dart
+++ lib/features/authentication/domain/provider/check_box_provider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:period_tracker/features/authentication/domain/use_cases/use_cases.dart';
 
 class CheckBoxProvider extends StateNotifier<bool> {
   CheckBoxProvider() : super(false);
 
   void toggleCheckbox() {
-    state = !state;
+    state = NewUserUseCases.toggleCheckBox(state);
   }
 }
